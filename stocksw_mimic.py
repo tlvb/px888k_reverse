06a96632fd18909e8a733948a17e44bbbcb598a0
Leo Bärring <leo.barring@gmail.com>
2016-09-10 21:24:05 +0200

    now possible to read from radio, to stdout/hex or file/bin
    Signed-off-by: Leo Bärring <leo.barring@gmail.com>

--- stocksw_mimic.py
+++ stocksw_mimic.py
@@ -24,39 +24,72 @@ blocksize = 64
 lowerrbound = 0
 upperrbound = 0x1000
 
+def mkreadcmd(a):
+    return bytearray([82, (a&0xff00)>>8, (a&0x00ff), blocksize])
+def mkwritecmd(a):
+    return bytearray([87, (a&0xff00)>>8, (a&0x00ff), blocksize])
+
 if len(sys.argv) == 2:
     rpath = sys.argv[1]
     if os.path.exists(rpath) and stat.S_ISCHR(os.stat(rpath).st_mode):
-        mode = 'sh' # serial to hexdump
+        mode = 'SH' # serial to hexdump
 elif len(sys.argv) == 3:
     rpath = sys.argv[1]
     wpath = sys.argv[2]
     if os.path.exists(rpath):
         if stat.S_ISCHR(os.stat(rpath).st_mode) and (not os.path.exists(wpath) or stat.S_ISREG(os.stat(wpath).st_mode)):
-            mode = 'sf' # serial to file
+            mode = 'SF' # serial to file
         elif stat.S_ISREG(os.stat(rpath).st_mode) and stat.S_ISCHR(os.stat(wpath).st_mode):
-            mode = 'fs' # file to serial
+            mode = 'FS' # file to serial
 
-if mode not in ['sh', 'sf', 'fs']:
+if mode not in ['SH', 'SF', 'FS']:
     print(descr)
     exit(1)
 
-print(">>> mode: {}".format(mode))
-if mode[0] == 's': # read from radio
-    print("foo")
+print(">>> MODE: {}".format(mode))
+s = None
+f = None
+if mode[0] == 'S': # read from radio
     s = serial.Serial(rpath, 9600, 8, serial.PARITY_NONE, 1)
-    print("bar")
-    f = sys.stdout
-    if mode[1] == 'f':
+    if mode[1] == 'F':
         f = open(wpath, 'wb')
     s.write(matchoutput)
     x = s.read(len(matchinput))
     if x == matchinput:
+        print(">>> HANDSHAKE OK".format(x, matchinput))
         for blockaddr in range(lowerrbound, upperrbound, blocksize):
-            print("{:04x}|".format(blockaddr))
+            s.write(mkreadcmd(blockaddr))
+            x = s.read(4)
+            if x == mkwritecmd(blockaddr):
+                block = s.read(blocksize)
+                if mode[1] == 'H':
+                    for sba in range(0, blocksize, 16):
+                        if sba == 0:
+                            print('{:04x} {:04x} |'.format(blockaddr, blockaddr+sba), end='')
+                        else:
+                            print('     {:04x} |'.format(blockaddr+sba), end='')
+                        for sbo in range(16):
+                            print(' {:02x}'.format(block[sba+sbo]), end='')
+                        print(' | ', end='')
+                        for sbo in range(16):
+                            if block[sba+sbo] in range(0x20,0x7f):
+                                print('{:c}'.format(block[sba+sbo]), end='')
+                            else:
+                                print('.', end='')
+                        print(' |')
+                else:
+                    f.write(block)
+            else:
+                print(">>> BAD COMMAND STRING actual:{}, expected:{}".format(x, mkwritecmd(blockaddr)))
+
     else:
         print(">>> BAD MATCH STRING actual:{}, expected:{}".format(x, matchinput))
-    s.close()
-    if mode[1] == 'f':
-        f.close()
+s.write(b'E')
+x = s.read(1)
+if x != b'\x06':
+    print(">>> BAD FINAL ACK BYTE actual: {}, expected: {}".format(x, b'\x06'))
+s.close()
+if 'F' in mode:
+    f.close()
+print(">>> DONE")
 
